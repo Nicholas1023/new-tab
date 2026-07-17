ed873f325a3a29bb6ec50a3c953fe38cdba08bf3
Nicholas Lim <nicholas_lim@bbsshack.club>
2026-07-17 12:30:23 +0800

    Add footer for attribution

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ let month = date.getMonth() + 1;
 
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${year}-${month}-${day}`).then(response => response.json()).then(data => {
     document.body.background=data.url;
+    console.log(data)
+    document.getElementById("imageCopyright").textContent=`Attribution: Background image from NASA APOD API. Image is © ${data.copyright}`;
 });
 
 let searchEngine = document.getElementById("searchURL");
